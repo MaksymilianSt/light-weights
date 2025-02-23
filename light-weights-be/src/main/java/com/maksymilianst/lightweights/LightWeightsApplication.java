3fe5e91eced3a153d63dd6cf44ef7bad8f8c43c0
Maks <Maksymilianst02@gmail.com>
2025-02-23 16:17:50 +0100

    setup BE

---
+++ light-weights-be/src/main/java/com/maksymilianst/lightweights/LightWeightsApplication.java
@@ -0,0 +1,13 @@
+package com.maksymilianst.lightweights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightWeightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightWeightsApplication.class, args);
+    }
+
+}
---
+++ light-weights-be/src/test/java/com/maksymilianst/lightweights/LightWeightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksymilianst.lightweights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightWeightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
