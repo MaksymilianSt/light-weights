dca6dd5c461ce7c68bc4df48e53e1cc200b4c813
Maks <Maksymilianst02@gmail.com>
2025-08-14 20:03:14 +0200

    BE:  update default rpe

--- light-weights-be/src/main/java/com/maksymilianst/lightweights/execution/service/impl/TrainingExecutionServiceImpl.java
+++ light-weights-be/src/main/java/com/maksymilianst/lightweights/execution/service/impl/TrainingExecutionServiceImpl.java
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class TrainingExecutionServiceImpl implements TrainingExecutionService {
-    private final static BigDecimal DEFAULT_RPE = BigDecimal.valueOf(5);
+    private final static BigDecimal DEFAULT_RPE = BigDecimal.valueOf(8);
 
     private final TrainingExecutionRepository trainingExecutionRepository;
     private final TrainingRepository trainingRepository;
